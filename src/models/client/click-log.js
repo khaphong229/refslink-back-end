c3820af47ab52ea5cb6509f29c324734674cfb92
sekido88 <161274267+sekido88@users.noreply.github.com>
2025-06-05 22:12:17 +0700

    add(goLink): click-logs

---
+++ src/models/client/click-log.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const ClickLogSchema = new mongoose.Schema({
+  link_id: { type: mongoose.Schema.Types.ObjectId, ref: 'ShortenLink', required: true },
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
+  ip: { type: String },
+  country: { type: String },
+  device: { type: String },
+  browser: { type: String },
+  referer: { type: String },
+  is_valid: { type: Boolean, default: true },
+  earned_amount: { type: Number, default: 0 },
+  created_at: { type: Date, default: Date.now },
+});
+
+export default mongoose.model('ClickLog', ClickLogSchema);
